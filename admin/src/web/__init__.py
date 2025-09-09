d56c1f151f5cb0f8cd8f4a4a7a7713f1061d649e
MauricioFRR <mauri030901@gmail.com>
2025-09-09 17:41:59 -0300

    Manejador de errores 404,401 y 500

---
+++ admin/src/web/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "No se encontro    el recurso"}), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({"error": "El servidor sufrio un error interno"}), 500
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({"error": "No tiene autorizacion"}), 401
