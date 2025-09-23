047323a9a3972c560ce4016c5471e737e8de8c32
Belen Desvoignes <belendesvoignes@gmail.com>
2025-09-22 22:37:51 -0300

    Se corrige la ruta de plantillas

--- admin/main.py
+++ admin/main.py
@@ -1,24 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 from src.web.controllers.admin_routes import admin_bp
 from src.web.config import config
 from src.core.database import db
 
 def create_app(env='development'):
-    app = Flask(__name__, static_folder='src/web/static')
+    # configura la carpeta de archivos estáticos y la de plantillas.
+    app = Flask(__name__, 
+                static_folder='src/web/static',
+                template_folder='src/web/templates')
     
-    #cargar la configuracion
+    #carga la configuración del ambiente.
     app.config.from_object(config[env])
     
-    #inicializar la bd
+    #inicializa la base de datos con la aplicación.
     db.init_app(app)
     
-    #registrar el blueprint
+    #registra el blueprint de las rutas de administración.
     app.register_blueprint(admin_bp)
+    
+    #define la ruta para la página principal.
+    @app.route("/")
+    def index():
+        return render_template("home.html")
     
     return app
 
 if __name__ == "__main__":
     app = create_app()
     with app.app_context():
+        #crea todas las tablas en la base de datos.
         db.create_all()
     app.run(debug=True)
