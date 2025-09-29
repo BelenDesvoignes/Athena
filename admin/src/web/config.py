1c1296694ad7d19171eebc5ff2cbacc30735d010
Belen Desvoignes <belendesvoignes@gmail.com>
2025-09-28 23:20:54 -0300

    Arreglo en la conexion a bd

--- admin/src/web/config.py
+++ admin/src/web/config.py
@@ -20,17 +20,20 @@ class ProductionConfig(Config):
 
 
 class DevelopmentConfig(Config):
-    SECRET_KEY = "your_development_secret_key"
-    DB_USER =  environ.get("DB_USER") or "postgres"
-    DB_PASSWORD = environ.get("DB_PASSWORD") or "postgres"
-    DB_HOST = environ.get("DB_HOST") or "localhost"
-    DB_PORT = environ.get("DB_PORT") or "5432"
-    DB_NAME = environ.get("DB_NAME") or "grupo19"
+    SECRET_KEY = "your_dev_secret_key"
+    DB_USER = "postgres"
+    DB_PASSWORD = "admin"
+    DB_HOST = "localhost"
+    DB_PORT = "5432"
+    DB_NAME = "grupo19"
     DB_SCHEME = "postgresql+psycopg2"
 
     SQLALCHEMY_DATABASE_URI = (
         f"{DB_SCHEME}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
+
+
+
 
 
 class TestingConfig(Config):
