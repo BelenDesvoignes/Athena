fe6779293debac44e6d13f40c59675a73dec790b
Belen Desvoignes <belendesvoignes@gmail.com>
2025-10-07 16:17:22 -0300

    Agrego reseteo de bd y arreglos en templates

--- admin/main.py
+++ admin/main.py
@@ -1,10 +1,13 @@
 from src.web import create_app
-from src.core.database import db
+from src.core.database import db, reset_db
 from src.core.seeds import seed_roles_permissions, seed_admin_user 
 
 app = create_app()
 
 with app.app_context():
+    print("Limpieza de bd")
+    reset_db()
+
     print("Iniciando creación de tablas y siembra de datos...")
     db.create_all()
     seed_roles_permissions() 
